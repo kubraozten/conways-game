9a85e4c42cf994340c38bd0c751dcfa2bed1c231
kubra <kubraozten@hotmail.com>
2020-01-20 18:02:18 +0300

    Empty Main Function and GridSize added.

---
+++ conways-game.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+import numpy as np
+
+# set grid size
+N = 6
+randomGrid = np.random.randint(0, 2, (N*100, N*100))
+newGrid = np.zeros(randomGrid.shape)
+
+def main():
+
+
+if __name__ == "__main__":
+    main()
+
